988794316ab77adde24e0e371df93d22b432131b
Harry-Gao-H <59434153+Harry-Gao-H@users.noreply.github.com>
2021-04-01 13:12:22 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -12,10 +12,17 @@ const routes = [
   {
     path: '/menu',
     name: 'Menu',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import('../views/Menu.vue')
+  },
+  {
+    path: '/login-staff',
+    name: 'LoginStaff',
+    component: () => import('../views/LoginStaff.vue')
+  },
+  {
+    path: '/login-student',
+    name: 'LoginStudent',
+    component: () => import('../views/LoginStudent.vue')
   },
   {
     path: '/product',
