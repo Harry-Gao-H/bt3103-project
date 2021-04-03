37528af1765c0e6380c6c4a5a1ee4fb6b8167a92
fooshiyun <shiyunfoo10@gmail.com>
2021-04-03 21:55:36 +0800

    Updated index.js
    Added link for profile page

--- src/router/index.js
+++ src/router/index.js
@@ -65,6 +65,15 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import('../views/Cart.vue')
   },
+
+  {
+    path: '/profile',
+    name: 'Profile',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/Profile.vue')
+  },
 ]
 
 const router = new VueRouter({
