e3ea4934901fe2668660c0508976fa955c7e3212
dylan <dylnchan@gmail.com>
2021-04-03 12:23:21 +0800

    Update Design StaffPage.vue

--- src/views/StaffPage.vue
+++ src/views/StaffPage.vue
@@ -1,16 +1,14 @@
 <template>
-
-    <div>
-        <section>
-            <nav>
-                <p>
-                     <br><router-link to="/">Dashboard</router-link>
-                  <br><router-link to="/">Logout</router-link>
-                </p>
-            </nav>
+<div>
+<div class ="navbar">
+ <ul>
+                    <li><router-link to="/cart">Dashboard</router-link></li>
+                    <li><router-link to="/">Logout</router-link></li>
+                    
+                </ul>
+</div>
+<div class="content">
         
-            <div id="content">
-                <div id = "profile">
 
  <table>
                         <tr>
@@ -29,16 +27,10 @@
 
                     </table>
 
-                        <!--TEST-->
-
-                </div>
-           
-            </div>
-        </section>
-        
+                     
  
     </div> 
-
+</div>
 </template>
 
 
@@ -85,100 +77,21 @@ export default {
 
 
 <style scoped>
-
-* {
-  box-sizing: border-box;
-}
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-/* Style the header */
-header {
-  background-color: #3895D3;
-  padding: 20px;
-  text-align: center;
-  color: black;
-  line-height: 0.3;
-}
-
-#subtitle {
-  font-family: Monospace;
-  font-size: 25px;
-}
-
-#title {
-  font-family: "Times New Roman", serif;
-  font-size: 50px;
- 
-}
-
-/* Create two columns that float next to each other */
-/*navigation column*/
-nav {
-  float: left;
-  width: 15%;
-  background-color: white;
-  padding-right: 20px;
-  height: 300px;
-}
-
-/* Style the paragraph inside the navigation */
-nav p {
-  text-align: center;
-  padding-top: 100px;
-  font-size: 25px;
-  line-height: 2.5;
-}
-
-/* Style the main content column beside navigation*/
-#content {
-  float: left;
-  padding: 35px;
-  width: 85%;
-  background-color: #f1f1f1;
-}
-
-/* Clear floats after the columns */
-section:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-/* Style the footer */
-footer {
-  background-color: #777;
-  padding: 10px;
-  text-align: center;
-  color: white;
-  line-height: 0.5;
-}
-
-a:hover {
-  color: red;
-}
-
 table {
-  border: 1px solid black;
-  width: 70%;
-  margin-left: 80px;
+  border-collapse: collapse;
+  width: 70%; 
+  margin-left: auto;
+  margin-right: auto;
 }
 
-td {
-  border: 1px solid black;
-  text-align: center;
-  padding: 15px;
-  font-size: 20px;
-  line-height: 1.5;
+td, th {
+  border: 4px solid #dddddd;
+  text-align: left;
+  padding: 28px;
+  font-size: 16px;
 }
-
-
-th {
-  font-size: 22px;
-  border: 1px solid black;
-  height: 45px;
+tr:nth-child(even) {
+  background-color: #acff59ad;
 }
 
 #request {
@@ -188,7 +101,6 @@ th {
 }
 
 
-
 #itemsList{
     width: 100%;
     max-width: 1200px;
@@ -196,20 +108,7 @@ th {
     padding: 0 5px;
     box-sizing: border-box;
 }
-ul{
-    display: flex;
-    flex-wrap: wrap;
-    list-style-type: none;
-    padding: 0;
-}
-li{
-    flex-grow: 1;
-    flex-basis: 300px;
-    text-align: center;
-    padding: 10px;
-    border: 1px solid #222;
-    margin: 10px;
-}
+
 img{
   width:150px;
   height: 150px;
