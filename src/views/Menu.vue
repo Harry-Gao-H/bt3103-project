59036d5442bcdfdd552c44f1716313715e2e296e
dylan <dylnchan@gmail.com>
2021-04-03 12:22:57 +0800

    Update Design Menu.vue

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,7 +1,17 @@
 <template>
 <div>
+<div class ="navbar">
+ <ul>
+                    <li><router-link to="/cart">Profile</router-link></li>
+                    <li><router-link to="/cart">Cart</router-link></li>
+                    <li><router-link to="/">Logout</router-link></li>
+                    
+                </ul>
+</div>
 
+<div id="content">
    <PageContent v-bind:itemsList="itemsListProps"></PageContent>
+</div>
 </div>
 </template>
 
@@ -41,14 +51,14 @@ export default {
           id: "#025",
           name: "Dry Beef Hor Fun",
           imageURL:
-            "https://delishar.com/wp-content/uploads/2016/04/Dry-Beef-Hor-Fun-3.jpg",
+            "https://3.bp.blogspot.com/-SMxoySoXcK0/T696enjIijI/AAAAAAAACoc/A5bFWd024KY/s1600/IMG_5140.JPG",
     
         },
         {
           id: "#067",
           name: "Sambal KangKung",
           imageURL:
-            "https://minikitchenlab.com/wp-content/uploads/2017/09/Kangkung-Belacan-e1506580822679-1300x1300.jpg",
+            "https://3.bp.blogspot.com/-SMxoySoXcK0/T696enjIijI/AAAAAAAACoc/A5bFWd024KY/s1600/IMG_5140.JPG",
           show: false,
       
         },
@@ -56,7 +66,7 @@ export default {
           id: "#077",
           name: "Pork Fried Rice",
           imageURL:
-            "https://hazeldiary.com/wp-content/uploads/2020/08/King-of-Fried-Rice-Golden-Mile-Tower-XO-Fried-Rice.jpg",
+            "https://3.bp.blogspot.com/-SMxoySoXcK0/T696enjIijI/AAAAAAAACoc/A5bFWd024KY/s1600/IMG_5140.JPG",
           show: false,
   
         },
@@ -64,7 +74,7 @@ export default {
           id: "#099",
           name: "Mapo Tofu",
           imageURL:
-            "https://i2.wp.com/seonkyounglongest.com/wp-content/uploads/2020/09/Mapo-Tofu-12-minijpg.jpg?fit=1000%2C1500&ssl=1",
+            "https://3.bp.blogspot.com/-SMxoySoXcK0/T696enjIijI/AAAAAAAACoc/A5bFWd024KY/s1600/IMG_5140.JPG",
           show: false,
   
         },
@@ -72,7 +82,7 @@ export default {
           id: "#200",
           name: "Cereal Prawn",
           imageURL:
-            "https://i.pinimg.com/originals/ad/87/eb/ad87eb2113e4d6480a21acdbb5f36114.jpg",
+            "https://3.bp.blogspot.com/-SMxoySoXcK0/T696enjIijI/AAAAAAAACoc/A5bFWd024KY/s1600/IMG_5140.JPG",
           show: false,
         },
       ]
@@ -96,36 +106,9 @@ export default {
 
 <style scoped>
 
-
-h1{
-    color:ivory
+#content {
+  width: 100%;
+  background-color: #f1f1f1;
 }
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#navigationBar {
-  float:right;
-}
-
-#dateDropdown {
-  float:left;
-  margin-top:250px;
-  margin-left:25px;
-  background-color:lightblue;
-}
-
-#mealDropdown {
-  float:left;
-  margin-top:250px;
-  margin-left:50px;
-  background-color:lightblue;
-}
-
 
 </style>
