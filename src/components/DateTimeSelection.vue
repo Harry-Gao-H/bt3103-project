7397d42f065b17e779c78474bfdcdc147eb1cbe6
dylan <dylnchan@gmail.com>
2021-04-03 21:17:46 +0800

    Updated components folder

---
+++ src/components/DateTimeSelection.vue
@@ -0,0 +1,164 @@
+<template>
+  <div id="dropdown">
+	<form>
+		<div>
+			<label for="meal">Choose your preferred date and time:</label>
+			<input id="meal" type="date" name="meal" min="2021-04-14" max="2021-04-20" v-model="selectedDate" v-on:change="fetch()" required>
+			{{selectedDate}}
+			
+			<br>
+			<input type="radio" class="radio" id="breakfast" name="breakfast-or-dinner" value="Breakfast" v-model="selectedMeal" v-on:change="fetch()"> 
+			<label for="breakfast" class="radio">Breakfast</label> 
+			<input type="radio" class="radio" id="dinner" name="breakfast-or-dinner" value="Dinner" v-model="selectedMeal" v-on:change="fetch()"> 
+			<label for="dinner" class="radio">Dinner</label>
+			{{selectedMeal}}
+		</div>
+	</form>
+
+	<!--{{cuisines}}-->
+
+	<div>
+		<div v-show = "selectedMeal == 'Breakfast'">
+			<p>Breakfast Takeaway Time</p>
+			<select id="time" name="time" v-model="takeawayTime">
+				<option value="0730">07:30</option>
+				<option value="0800">08:00</option>
+				<option value="0830">08:30</option>
+				<option value="0900">09:00</option>
+				<option value="0930">09:30</option>
+			</select>
+		</div>
+
+		<div v-show = "selectedMeal == 'Dinner'">
+			<p>Dinner Takeaway Time</p>
+			<select id="time" name="time" v-model="takeawayTime">
+				<option value="1800">18:00</option>
+				<option value="1830">18:30</option>
+				<option value="1900">19:00</option>
+				<option value="1930">19:30</option>
+				<option value="2000">20:00</option>
+			</select>
+		</div>
+  
+	</div>
+
+	<br>
+	
+	<button v-on:click="showMenu=true" id="buttonClick">Check Menu</button>
+
+	<div v-show="showMenu">
+		<ul>
+			<li v-for="cuisine in cuisines" v-bind:key="cuisine[0]" >
+				<h2><u v-on:click="goToProductPage(cuisine)">{{cuisine[0]}}</u></h2>
+				
+				<p v-for="dish in cuisine[1].dishes" v-bind:key = "dish">
+					{{dish}}
+				</p>
+			</li>
+		</ul>
+	</div>
+	
+
+  </div>
+</template>
+
+<script>
+import database from "../firebase.js"
+
+export default {
+    props:{
+        itemsList: {
+            type: Array
+        } 
+    },
+	data() {
+		return {
+			selectedDate:'',
+			selectedMeal:'',
+			showMenu:false,
+			takeawayTime:'',
+			cuisines:[]
+		}
+	},
+
+	methods: {
+		fetchCuisines: function() {
+			//change the form of date
+			var strings = this.selectedDate.split("-")
+			var newDate = strings[2] + "-" + strings[1].substring(1,2) + "-" + strings[0]
+			
+			console.log(newDate)
+			console.log(this.selectedMeal)
+
+			this.cuisines=[]; // clear the cuisines
+			database.collection("Menu").doc(newDate)
+				.collection(this.selectedMeal).get().then(snapshot => {
+				snapshot.docs.forEach(doc => {
+						this.cuisines.push([doc.id,doc.data()])
+					})
+				})
+		},
+		fetch: function() {
+			//fetch cuisines from database when time and meal are selected
+			if (this.selectedDate != '' && this.selectedMeal != '') {
+				this.fetchCuisines()
+			}
+		},
+		goToProductPage:function(cuisine) {
+			this.$router.push( {name: "Product", params: {"cuisine": cuisine, "meal": this.selectedMeal}} )
+		}
+		,
+		f:function() {
+			// for test
+			alert("testing")
+		}
+	}
+	
+}
+
+
+</script>
+
+
+<style scoped>
+#dropdown {
+font-family: Avenir, Helvetica, Arial, sans-serif;
+
+}
+label {
+    display: block;
+    font: 1rem 'Fira Sans', sans-serif;
+}
+
+input,
+label {
+    margin: .4rem 0;
+}
+
+.radio { 
+	display: inline;
+	padding: 10px;
+}
+#itemsList{
+    width: 100%;
+    max-width: 1200px;
+    margin: 30px auto;
+    padding: 0 5px;
+    box-sizing: border-box;
+}
+
+ul {
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    flex-grow: 1;
+    flex-basis: 300px;
+    text-align: center;
+    padding: 10px;
+    border: 1px solid #222;
+    margin: 10px;
+}
+</style>
--- src/components/PageContent.vue
+++ src/components/PageContent.vue
@@ -2,20 +2,12 @@
 
 <div>
 
-    <nav>
-      <p>
-      <router-link to="/">Menu</router-link>
-      <br><router-link to="/cart">Cart</router-link>
-      </p>
-    </nav>
-
     <div id="content">
         <div id = "profile">
             <ul>
             <li> 
-              <center><DateDropdown v-bind:itemsList="itemsList"></DateDropdown></center>
+              <center><DateTimeSelection v-bind:itemsList="itemsList"></DateTimeSelection></center>
               <br>
-              <MealAndSectionDropdown v-bind:itemsList="itemsList"></MealAndSectionDropdown> 
             </li>
             </ul>
         </div>
@@ -27,8 +19,8 @@
 
 
 <script>
-import DateDropdown from './DateDropdown.vue'
-import MealAndSectionDropdown from './MealAndSectionDropdown.vue'
+import DateTimeSelection from './DateTimeSelection.vue'
+
 
 export default {
   props:{
@@ -38,118 +30,22 @@ export default {
   },
 
   components: {
-    'DateDropdown':DateDropdown,
-    'MealAndSectionDropdown':MealAndSectionDropdown,
+    'DateTimeSelection':DateTimeSelection,
   },
+
+  
 }
 </script>
 
 <style scoped>
 
-* {
-  box-sizing: border-box;
-}
-
-nav {
-  float: left;
-  width: 15%;
-  background-color: white;
-  padding-right: 20px;
-  height: 400px;
-}
-
-/* Style the paragraph inside the navigation */
-nav p {
-  text-align: center;
-  padding-top: 100px;
-  font-size: 25px;
-  line-height: 2.5;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
-
 #content {
-  padding: 35px;
-  width: 85%;
+  width: 100%;
   background-color: #f1f1f1;
 }
-
-/* Style the footer */
-footer {
-  background-color: #777;
-  padding: 10px;
-  text-align: center;
-  color: white;
-  line-height: 0.5;
+img{
+  height:50px;
+  width:50px;
 }
-
-
-table {
-  border: 1px solid black;
-  width: 70%;
-  margin-left: 80px;
-}
-
-td {
-  border: 1px solid black;
-  text-align: center;
-  padding: 15px;
-  font-size: 20px;
-  line-height: 1.5;
-}
-
-
-th {
-  font-size: 22px;
-  border: 1px solid black;
-  height: 45px;
-}
-
-#request {
-  font-size: 25px;
-  height: 45px;
-  background-color: #41b3a3;
-}
-
-#itemsList{
-    width: 100%;
-    max-width: 1200px;
-    margin: 30px auto;
-    padding: 0 5px;
-    box-sizing: border-box;
-}
-ul{
-    display: flex;
-    flex-wrap: wrap;
-    list-style-type: none;
-    padding: 0;
-}
-li{
-    flex-grow: 1;
-    flex-basis: 300px;
-    text-align: center;
-    padding: 10px;
-    border: 1px solid #222;
-    margin: 10px;
-}
-
-
-button{
-    font-weight:bold;
-    height:40px;
-    width:100px;
-    font-size:17px;
-    margin-left:500px;
-}
-
-center{
-  display:block;
-  margin-left:auto;
-  margin-right:auto;
-}
-
 
 </style>
