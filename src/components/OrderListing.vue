af6a324cb1732879618664b3faf7498afb951b2b
coolieKi <e0407823@u.nus.edu>
2021-04-09 14:42:45 +0800

    Create OrderListing.vue
    Created OrderListing Component

---
+++ src/components/OrderListing.vue
@@ -0,0 +1,63 @@
+<template>
+
+<div id="app">
+  <v-app id="inspire">
+    <v-data-table
+      :headers="headers"
+      :items="this.orders"
+      :single-expand="singleExpand"
+      :expanded.sync="expanded"
+      item-key="stud_id"
+      class="elevation-1"
+    >
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Order List</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-switch
+            v-model="singleExpand"
+            label="Single expand"
+            class="mt-2"
+          ></v-switch>
+        </v-toolbar>
+      </template>
+    </v-data-table>
+  </v-app>
+</div>
+</template>
+
+<script>
+export default {
+  name: "orderListing",
+  props: {
+    orders: {
+      type: Array,
+    }
+  },
+
+  data () {
+    return {
+      expanded: [],
+      singleExpand: false,
+      headers: [
+        {
+          text: 'Student ID',
+          align: 'start',
+          sortable: false,
+          value: 'stud_id',
+        },
+        { text: 'Collection Time', value: 'collect_time' },
+        { text: 'Meal Type', value: 'meal_type' },
+        { text: '', value: 'data-table-expand' },
+      ],
+    }
+  },
+  
+  watch: {}
+
+};
+</script>
+
+<style scoped>
+
+</style>
