03dd928c7d29d55630a41822b4179e46ee84a659
coolieKi <e0407823@u.nus.edu>
2021-04-09 16:13:08 +0800

    Edited OrderOverview
    To include order listing

--- src/views/OrdersOverview.vue
+++ src/views/OrdersOverview.vue
@@ -8,51 +8,82 @@
 </div>
 
 <div id="content">
-   <PageContentStaff v-bind:itemsList="itemsListProps"></PageContentStaff>
+   <order-listing :orders="orders" />
 </div>
 </div>
 </template>
 
 <script>
-import PageContentStaff from '@/components/PageContentStaff.vue'
+import orderListing from "../components/OrderListing"
 import database from "../firebase.js"
 import firebase from "firebase"
 
 export default {
   components: {
-    PageContentStaff
+    orderListing
 
   },
-  data() {
-    return {
-    }
-    
-  },
+  
+  data: () => ({
+    selectedDate: "10-4-2021",
+    selectedMeal: "Breakfast",  
+    orders: [],
+  }),
+
   methods: {
-    fetchItems:function() {
-      database.collection("Menu").doc("14-4-2021")
-        .collection("Breakfast").get().then(snapshot => {
-          snapshot.docs.forEach(doc => {
-            this.items.push(doc.data())
+    getOrders(date) {
+
+      var dict = {
+        stud_id: "",
+        collect_time: "",
+        meal_type: [],
+        special_need: []
+        };
+
+      if (this.selectedMeal == "Breakfast"){
+        this.orders = [];
+        database.collection("Order_test").doc(date)
+          .collection(this.selectedMeal).get().then(snapshot => {
+            snapshot.docs.forEach(doc => {
+              let copyDict= JSON.parse(JSON.stringify(dict))
+              copyDict["stud_id"] = doc.id,
+              copyDict["collect_time"] = doc.data().takeawayTiming,
+              copyDict["meal_type"] = doc.data().mealSelect,
+              this.orders.push(copyDict)
+            })
           })
-        })
-    },
-    logout() {
-      firebase
-        .auth()
-        .signOut()
-        .then(() => {
-          alert('Successfully logged out');
-          this.$router.push('/');
-        })
-        .catch(error => {
+          console.log(this.orders)
+      } else {
+        this.orders=[];
+        database.collection("Order_test").doc(date)
+          .collection(this.selectedMeal).get().then(snapshot => {
+            snapshot.docs.forEach(doc => {
+              dict.stud_id = doc.id,
+              dict.collect_time = doc.data().takeawayTiming,
+              dict.meal_type = doc.data().mealSelect,
+              this.orders.push(dict)
+              console.log(this.orders)
+                })
+              })  
+            }
+          },
+          logout() {
+            firebase
+            .auth()
+            .signOut()
+            .then(() => {
+            alert('Successfully logged out');
+            this.$router.push('/');
+            })
+          .catch(error => {
           alert(error.message);
           this.$router.push('/');
         });
     },
-  }
-  
-
+  },
+  created() {
+    this.getOrders(this.selectedDate);
+  },
 }
 </script>
 
