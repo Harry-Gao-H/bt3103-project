dc0f27b6b1b6b40b0cdcfbc76e10a41534ebcc8b
Harry-Gao-H <59434153+Harry-Gao-H@users.noreply.github.com>
2021-04-13 14:36:09 +0800

    History page added

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -5,6 +5,7 @@
         <ul>
             <li><router-link to="/cart">Cart</router-link></li>
             <li><router-link to="/menu">Menu</router-link></li>
+            <li><router-link to="/history">History</router-link></li>
             <li class="navbarstyle" v-on:click="logout()">Logout</li>       
         </ul>
     </div>
