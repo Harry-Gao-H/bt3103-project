6ef88d297b5f65d0c4689ef97d204604728e6eb1
dylan <dylnchan@gmail.com>
2021-04-14 19:17:50 +0800

    Resolved error for GroupBarChart in Dashboard

--- src/components/charts/GroupedBarChart.vue
+++ src/components/charts/GroupedBarChart.vue
@@ -1,44 +1,221 @@
-<template>
-  <div id="main">
-        <div id="chart">
-            <!--<bar-chart v-bind:itemsProps="itemsProps"></bar-chart>-->
-            <!-- <button v-on:click="clearData()"> Clear Data </button> -->
-            <!--<button v-on:click="updateData()"> Update Data </button> -->
-            <grouped-bar-chart ref="chart"></grouped-bar-chart>
-
-            
-        </div>
-        <!--<h1 v-for="item in this.itemsProps" v-bind:key="item">{{item}} </h1>-->
-    </div>
-</template>
 
 <script>
-import groupedbarchart from './GroupedBarChart.js'
+//import groupedbarchart from './GroupedBarChart.js'
+import database from '../../firebase.js'
+
+import {Bar} from 'vue-chartjs' 
+
 
 
 export default {
-  //  props:{
-  //       itemsProps: {
-  //           type: Array
-  //       } 
-  //   },
+    extends: Bar,
 
   components: {
-    'grouped-bar-chart': groupedbarchart
+    //'grouped-bar-chart': groupedbarchart
+
+
+	},
+
+  data: function () {
+    return {
+        datacollection: {
+            results:[],
+            labels: [],
+            datasets: [
+              {
+                label: "Total Orders", 
+                backgroundColor: ["#25668e", "#634171","#2c8773","#873f2c"],
+                data: [],
+                //hidden:true,
+              }, //35, 80
+
+              {
+                label: "Total orders with smaller portion", 
+                backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#c15b3e"],
+                data: [0,0,0,0]
+              },
+              {
+                label: "Total orders without smaller portion", 
+                backgroundColor: ['#aed3ea','#d2beda','#b2e6db','#e8c3b9',
+                ],
+                data: [0,0,0,0]
+              },
+
+            ]
+        },
+        options: {
+            legend: { display: false },
+            title: {
+              display: false,
+              text: 'Total Number of each cuisine'
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+        //     scales: {
+        //       yAxes: [{
+        //         stacked: true,
+        //         ticks: {
+        //           beginAtZero: true
+        //         }
+        //       }],
+        //       xAxes: [{
+        //         stacked: true,
+        //         ticks: {
+        //           beginAtZero: true
+        //         }
+        //       }]
+        // }
+
+        scales:{
+          yAxes:[{
+              ticks:{
+                  min:0,
+                  precision:0
+              },
+              //stacked:true
+           }],
+           xAxes: [{
+                    // stacked: true,
+                    ticks: {
+                      beginAtZero: true
+                    }
+                  }]
+      },
+
+      tooltips: {
+        mode: 'index',
+      },
+
+      plugins: {
+        labels: {
+          render: 'percentage',
+          
+        }
+      }
+
+      }
+    }
   },
-  
-  methods : {
-    clearData:function() {
-      //console.log("update")
-      this.$refs.chart.clearData()
-    },
+
+
+  methods: {
+
+
+    // clearData:function() {
+    //   this.datacollection.labels = []
+    //   this.datacollection.datasets[0].data = []
+    //   this.datacollection.datasets[1].data = [0,0,0,0]
+    //   this.datacollection.datasets[2].data = [0,0,0,0]
+
+    //   this.renderChart(this.datacollection, this.options)
+    // },
+
+               
     updateData:function(date, meal) {
-      this.$refs.chart.updateData(date, meal)
+      this.datacollection.labels = []
+      this.datacollection.datasets[0].data = []
+      this.datacollection.datasets[1].data = [0,0,0,0]
+      this.datacollection.datasets[2].data = [0,0,0,0]
+
+      database.collection('Order').doc(date)
+      .collection(meal).get().then(snapshot => {
+        snapshot.docs.forEach(doc => { 
+
+          for (var index in doc.data().orders) {
+
+            for (var order in doc.data().orders[index]) { 
+
+              if (order == "cuisine") {
+
+                for (var i in doc.data().orders[index][order]) {
+
+                  if (typeof doc.data().orders[index][order][i] == 'string') {
+                    if (!this.datacollection.labels.includes(doc.data().orders[index][order][i])) {
+                      this.datacollection.labels.push(doc.data().orders[index][order][i])
+                      this.datacollection.datasets[0].data.push(doc.data().orders[index]["quantity"])
+                    } else {
+                      var indexOfCuisine = this.datacollection.labels.indexOf(doc.data().orders[index][order][i]) 
+                      this.datacollection.datasets[0].data[indexOfCuisine] += (doc.data().orders[index]["quantity"])
+                      }
+                    }
+                }
+              }
+              }
+            }
+          })
+      })
+
+
+      database.collection('Order').doc(date)
+      .collection(meal).get().then(snapshot => {
+        snapshot.docs.forEach(doc => { 
+
+          for (var index in doc.data().orders) {
+
+            // console.log(doc.data().orders[index])
+
+            for (var order in doc.data().orders[index]) { 
+              // console.log(order)
+
+              if (order == "small") {
+
+
+
+                if (doc.data().orders[index]["small"]) {
+                  // console.log(doc.data().orders[index]["small"])
+                  // console.log(doc.data().orders[index])
+
+                  for (var i in doc.data().orders[index]) {
+                    if (i == "cuisine") {
+                      // console.log(doc.data().orders[index][i][0])
+                      if (this.datacollection.labels.includes(doc.data().orders[index][i][0])) {
+                        var index2 = this.datacollection.labels.indexOf(doc.data().orders[index][i][0])
+                        // console.log(index2)
+                        // console.log(doc.data().orders[index]["quantity"])
+                        this.datacollection.datasets[1].data[index2] += (doc.data().orders[index]["quantity"])
+
+                      }
+                    }
+                  }
+                }
+                  if (!doc.data().orders[index]["small"]) {
+                    for (var j in doc.data().orders[index]) {
+                      if (j=="cuisine") {
+                        if (this.datacollection.labels.includes(doc.data().orders[index][j][0])) {
+                          var index3 = this.datacollection.labels.indexOf(doc.data().orders[index][j][0])
+                          this.datacollection.datasets[2].data[index3] += doc.data().orders[index]["quantity"]
+                        }
+                      }
+                    }
+                  }
+
+              }
+             
+
+              }
+            }
+          })
+          this.renderChart(this.datacollection, this.options)
+
+      })
+
+
+
+
+
     },
-    // updateData2:function(date, meal) {
-    //   this.$refs.chart.updateData2(date, meal)
-    // }
-  }
+
+  }, 
+
+ 
+
+  // created () {
+  //   //console.log(this.itemsProps[0])
+  //   //console.log(this.itemsProps[1])
+  //   //this.fetchItems()
+  //   this.clearData()
+  // },
+
 };
 </script>
 
